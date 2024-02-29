b9bc17b2545dc245502f80ca977fe16e8f1e3817
Richard Harris <richardharris916@gmail.com>
2024-02-29 01:15:35 -0600

    Added nodemon for dev Updated demo for logging purposes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Battleship from './Battleship'
 import GameState from './enums/GameState'
 import ShipTypes from './enums/ShipTypes'
-import {Spaces} from './Board'
+import {Spaces, ShipCounts} from './Board'
 
 // export default Battleship
-export {Battleship, GameState, ShipTypes, Spaces}
+export {Battleship, GameState, ShipTypes, Spaces, ShipCounts}
